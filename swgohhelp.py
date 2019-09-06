c621db856ef85e6dd8036c1bff2ffa22e4e4bbfa
OMotornyi <motorny.sasha@gmail.com>
2019-09-07 01:16:39 +0200

    updated to use aiohttp

--- swgohhelp.py
+++ swgohhelp.py
@@ -1,19 +1,19 @@
-from utils2 import http_get, http_post, get_units_dict
-
+from utils2 import http_get, http_post, get_units_dict, aiohttp_post
+import aiohttp
 import json
 import sys
 from os import path
 from datetime import datetime, timedelta
 
 db = {
-	'stats':   {},
-	'players': {},
-	'guilds':  {},
-	'roster':  {},
-	'units':   {},
-	'events':  {},
-	'zetas':   {},
-	'data':    {},
+    'stats':   {},
+    'players': {},
+    'guilds':  {},
+    'roster':  {},
+    'units':   {},
+    'events':  {},
+    'zetas':   {},
+    'data':    {},
 }
 
 SWGOH_HELP = 'https://api.swgoh.help'
@@ -23,174 +23,213 @@ CRINOLO_TEST_URL = 'https://crinolo-swgoh.glitch.me/testCalc/api'
 CRINOLO_BETA_URL = 'https://crinolo-swgoh-beta.glitch.me/statCalc/api'
 
 if path.isfile("../CONFIGURE.json"):
-	with open ("../CONFIGURE.json") as json_file:
-		CONFIG=json.load(json_file)
+    with open ("../CONFIGURE.json") as json_file:
+        CONFIG=json.load(json_file)
 else:
-	 print("File does not exist")
+     print("File does not exist")
 
 #
 # Internal - Do not call yourself
 #
 
+async def ai_get_access_token(config):
+    if 'access_token' in config['swgoh.help']:
+        expire = config['swgoh.help']['access_token_expire']
+        if expire > datetime.now() + timedelta(seconds=60):
+            return config['swgoh.help']['access_token']
+
+    headers = {
+        'method': 'post',
+        'content-type': 'application/x-www-form-urlencoded',
+    }
+
+    data = {
+        'username': config['swgoh.help']['username'],
+        'password': config['swgoh.help']['password'],
+        'grant_type': 'password',
+        'client_id': 'abc',
+        'client_secret': '123',
+    }
+
+    auth_url = '%s/auth/signin' % SWGOH_HELP
+    async with aiohttp.ClientSession() as session:
+        response, error = await aiohttp_post(session,auth_url, headers=headers, data=data)
+        if error:
+            raise Exception('Authentication failed to swgohhelp API: %s' % error)
+    data = await response.json()
+    if 'access_token' not in data:
+        raise Exception('Authentication failed: Server returned `%s`' % data)
+
+    config['swgoh.help']['access_token'] = data['access_token']
+    config['swgoh.help']['access_token_expire'] = datetime.now() + timedelta(seconds=data['expires_in'])
+
+    return config['swgoh.help']['access_token']
+
+
 def get_access_token(config):
 
-	if 'access_token' in config['swgoh.help']:
-		expire = config['swgoh.help']['access_token_expire']
-		if expire > datetime.now() + timedelta(seconds=60):
-			return config['swgoh.help']['access_token']
+    if 'access_token' in config['swgoh.help']:
+        expire = config['swgoh.help']['access_token_expire']
+        if expire > datetime.now() + timedelta(seconds=60):
+            return config['swgoh.help']['access_token']
 
-	headers = {
-		'method': 'post',
-		'content-type': 'application/x-www-form-urlencoded',
-	}
+    headers = {
+        'method': 'post',
+        'content-type': 'application/x-www-form-urlencoded',
+    }
 
-	data = {
-		'username': config['swgoh.help']['username'],
-		'password': config['swgoh.help']['password'],
-		'grant_type': 'password',
-		'client_id': 'abc',
-		'client_secret': '123',
-	}
+    data = {
+        'username': config['swgoh.help']['username'],
+        'password': config['swgoh.help']['password'],
+        'grant_type': 'password',
+        'client_id': 'abc',
+        'client_secret': '123',
+    }
 
-	auth_url = '%s/auth/signin' % SWGOH_HELP
-	response, error = http_post(auth_url, headers=headers, data=data)
-	if error:
-		raise Exception('Authentication failed to swgohhelp API: %s' % error)
+    auth_url = '%s/auth/signin' % SWGOH_HELP
+    response, error = http_post(auth_url, headers=headers, data=data)
+    if error:
+        raise Exception('Authentication failed to swgohhelp API: %s' % error)
 
-	data = response.json()
-	if 'access_token' not in data:
-		raise Exception('Authentication failed: Server returned `%s`' % data)
+    data = response.json()
+    if 'access_token' not in data:
+        raise Exception('Authentication failed: Server returned `%s`' % data)
 
-	config['swgoh.help']['access_token'] = data['access_token']
-	config['swgoh.help']['access_token_expire'] = datetime.now() + timedelta(seconds=data['expires_in'])
+    config['swgoh.help']['access_token'] = data['access_token']
+    config['swgoh.help']['access_token_expire'] = datetime.now() + timedelta(seconds=data['expires_in'])
 
-	return config['swgoh.help']['access_token']
+    return config['swgoh.help']['access_token']
 
-def get_headers(config):
-	return {
-		'method': 'post',
-		'content-type': 'application/json',
-		'authorization': 'Bearer %s' % get_access_token(config),
-	}
+async def get_headers(config):
+    token = await ai_get_access_token(config)
+    return {
+        'method': 'post',
+        'content-type': 'application/json',
+        'authorization': 'Bearer %s' % token,
+    }
 
-def call_api(config, project, url):
-	headers = get_headers(config)
+async def call_api(config, project, url):
+    headers =await get_headers(config)
 
-	#if config['debug'] is True:
+    #if config['debug'] is True:
 #		print("CALL API: %s %s %s" % (url, headers, project), file=sys.stderr)
 
-	response, error = http_post(url, headers=headers, json=project)
-	if error:
-		raise Exception('http_post(%s) failed: %s' % (url, error))
+    async with aiohttp.ClientSession() as session:
+        response, error = await aiohttp_post(session,url, headers=headers,json=project)
+        if error:
+            raise Exception('http_post(%s) failed: %s' % (url, error))
 
-	try:
-		data = response.json()
+    try:
+        data =await response.json()
 
-	except Exception as err:
-		print("Failed to decode JSON:\n%s\n---" % response.content)
-		raise err
+    except Exception as err:
+        print("Failed to decode JSON:\n%s\n---" % response.content)
+        raise err
 
-	if 'error' in data and 'error_description' in data:
-		raise Exception(data['error_description'])
+    if 'error' in data and 'error_description' in data:
+        raise Exception(data['error_description'])
 
-	return data
+    return data
 
 #
 # API
 #
 
-def api_swgoh_players(config, project):
+async def api_swgoh_players(config, project):
 
-	result = []
-	expected_players = len(project['allycodes'])
+    result = []
+    expected_players = len(project['allycodes'])
 
-	new_proj = dict(project)
-	new_proj['allycodes'] = list(project['allycodes'])
+    new_proj = dict(project)
+    new_proj['allycodes'] = list(project['allycodes'])
 
-	while len(result) < expected_players:
+    while len(result) < expected_players:
 
-		returned = call_api(config, new_proj, '%s/swgoh/players' % SWGOH_HELP)
-		for player in returned:
-			result.append(player)
-			new_proj['allycodes'].remove(player['allyCode'])
+        returned =await call_api(config, new_proj, '%s/swgoh/players' % SWGOH_HELP)
+        for player in returned:
+            result.append(player)
+            new_proj['allycodes'].remove(player['allyCode'])
 
-	return result
+    return result
 
-def api_swgoh_guilds(config, project):
-	return call_api(config, project, '%s/swgoh/guilds' % SWGOH_HELP)
+async def api_swgoh_guilds(config, project):
+    return await call_api(config, project, '%s/swgoh/guilds' % SWGOH_HELP)
 
-def api_swgoh_roster(config, project):
-	return call_api(config, project, '%s/swgoh/roster' % SWGOH_HELP)
+async def api_swgoh_roster(config, project):
+    return await call_api(config, project, '%s/swgoh/roster' % SWGOH_HELP)
 
-def api_swgoh_units(config, project):
-	return call_api(config, project, '%s/swgoh/units' % SWGOH_HELP)
+async def api_swgoh_units(config, project):
+    return await call_api(config, project, '%s/swgoh/units' % SWGOH_HELP)
 
-def api_swgoh_zetas(config, project):
-	return call_api(config, project, '%s/swgoh/zetas' % SWGOH_HELP)
+async def api_swgoh_zetas(config, project):
+    return await call_api(config, project, '%s/swgoh/zetas' % SWGOH_HELP)
 
-def api_swgoh_squads(config, project):
-	return call_api(config, project, '%s/swgoh/squads' % SWGOH_HELP)
+async def api_swgoh_squads(config, project):
+    return await call_api(config, project, '%s/swgoh/squads' % SWGOH_HELP)
 
-def api_swgoh_events(config, project):
-	return call_api(config, project, '%s/swgoh/events' % SWGOH_HELP)
+async def api_swgoh_events(config, project):
+    return await call_api(config, project, '%s/swgoh/events' % SWGOH_HELP)
 
-def api_swgoh_data(config, project):
-	return call_api(config, project, '%s/swgoh/data' % SWGOH_HELP)
+async def api_swgoh_data(config, project):
+    return await call_api(config, project, '%s/swgoh/data' % SWGOH_HELP)
 
 def api_crinolo(config, units):
-	url = '%s?flags=gameStyle' % CRINOLO_PROD_URL
+    url = '%s?flags=gameStyle' % CRINOLO_PROD_URL
 
-	if config['debug'] is True:
-		print('CALL CRINOLO API: %s' % url, file=sys.stderr)
+    if config['debug'] is True:
+        print('CALL CRINOLO API: %s' % url, file=sys.stderr)
 
-	response, error = http_post(url, json=units)
-	if error:
-		raise Exception('http_post(%s) failed: %s' % (url, error))
+    response, error = http_post(url, json=units)
+    if error:
+        raise Exception('http_post(%s) failed: %s' % (url, error))
 
-	data = response.json()
-	if 'error' in data:
-		raise Exception('POST request failed for URL: %s\n%d Error: %s' % (url, response.status_code, data['error']))
+    data = response.json()
+    if 'error' in data:
+        raise Exception('POST request failed for URL: %s\n%d Error: %s' % (url, response.status_code, data['error']))
 
-	return data
+    return data
 #
 # Fetch functions
 #
 
-def fetch_players(config, project):
+async def fetch_players(config, project):
 
-	if type(project) is list:
-		project = { 'allycodes': project }
+    if type(project) is list:
+        project = { 'allycodes': project }
 
-	players = api_swgoh_players(config, project)
+    players = await api_swgoh_players(config, project)
 
-	result = {}
-	for player in players:
+    result = {}
+    for player in players:
 
-		if 'roster' in player:
-			player['roster'] = get_units_dict(player['roster'], 'defId')
+        if 'roster' in player:
+            player['roster'] = get_units_dict(player['roster'], 'defId')
 
-		ally_code = player['allyCode']
-		result[ally_code] = player
+        ally_code = player['allyCode']
+        result[ally_code] = player
 
-	return result
+    return result
 
-def fetch_guilds(config, project):
+async def fetch_guilds(config, project):
 
-	if type(project) is list:
-		project = { 'allycodes': project }
+    if type(project) is list:
+        project = { 'allycodes': project }
 
-	ally_codes = project['allycodes']
-	guilds = api_swgoh_guilds(config, project)
+    ally_codes = project['allycodes']
+    guilds = await api_swgoh_guilds(config, project)
+    print(guilds)
+    print(project)
+    #return guilds
+    result = {}
+    for guild in guilds:
 
-	result = {}
-	for guild in guilds:
+        guild['roster'] = get_units_dict(guild['roster'], 'allyCode')
+        for ally_code in ally_codes:
+            print(ally_code)
+            if ally_code in guild['roster']:
+                result[ally_code] = guild
 
-		guild['roster'] = get_units_dict(guild['roster'], 'allyCode')
-		for ally_code in ally_codes:
-			if ally_code in guild['roster']:
-				result[ally_code] = guild
-
-	return result
+    return result
 
 
 
--- utils2.py
+++ utils2.py
@@ -1,104 +1,105 @@
 # -*- coding: utf-8 -*-
-
+import aiohttp
 import os
 import pytz
 import requests
 import subprocess
-from requests.exceptions import HTTPError
+#from requests.exceptions import HTTPError
+from aiohttp.web_exceptions import HTTPError
 from datetime import datetime, timedelta
 from decimal import Decimal, ROUND_HALF_UP
 
 SQUAD_ROLES = {
-	1: 'Member',
-	2: 'Leader',
-	3: 'Commander',
-	5: 'Reinforcement',
+    1: 'Member',
+    2: 'Leader',
+    3: 'Commander',
+    5: 'Reinforcement',
 }
 
 PERCENT_STATS = [
-	'%armor',
-	'%resistance',
-	'%physical-critical-chance',
-	'%special-critical-chance',
-	'%critical-damage',
-	'%potency',
-	'%tenacity',
+    '%armor',
+    '%resistance',
+    '%physical-critical-chance',
+    '%special-critical-chance',
+    '%critical-damage',
+    '%potency',
+    '%tenacity',
 ]
 
 FORMAT_LUT = {
-	'%gear':   'gear',
-	'%gp':     'gp',
-	'%level':  'level',
-	'%rarity': 'starLevel',
-	'%zetas':  'zetas',
+    '%gear':   'gear',
+    '%gp':     'gp',
+    '%level':  'level',
+    '%rarity': 'starLevel',
+    '%zetas':  'zetas',
 }
 
 STATS_LUT = {
-	'%health':                      'Health',
-	'%strength':                    'Strength',
-	'%agility':                     'Agility',
-	'%tactics':                     'Tactics',
-	'%speed':                       'Speed',
-	'%physical-damage':             'Physical Damage',
-	'%special-damage':              'Special Damage',
-	'%armor':                       'Armor',
-	'%resistance':                  'Resistance',
-	'%armor-penetration':           'Armor Penetration',
-	'%resistance-penetration':      'Resistance Penetration',
-	'%dodge-chance':                'Dodge Chance',
-	'%deflection-chance':           'Deflection Chance',
-	'%physical-critical-chance':    'Physical Critical Chance',
-	'%special-critical-chance':     'Special Critical Chance',
-	'%critical-damage':             'Critical Damage',
-	'%potency':                     'Potency',
-	'%tenacity':                    'Tenacity',
-	'%health-steal':                'Health Steal',
-	'%protection':                  'Protection',
-	'%physical-accuracy':           'Physical Accuracy',
-	'%special-accuracy':            'Special Accuracy',
-	'%physical-critical-avoidance': 'Physical Critical Avoidance',
-	'%special-critical-avoidance':  'Special Critical Avoidance',
+    '%health':                      'Health',
+    '%strength':                    'Strength',
+    '%agility':                     'Agility',
+    '%tactics':                     'Tactics',
+    '%speed':                       'Speed',
+    '%physical-damage':             'Physical Damage',
+    '%special-damage':              'Special Damage',
+    '%armor':                       'Armor',
+    '%resistance':                  'Resistance',
+    '%armor-penetration':           'Armor Penetration',
+    '%resistance-penetration':      'Resistance Penetration',
+    '%dodge-chance':                'Dodge Chance',
+    '%deflection-chance':           'Deflection Chance',
+    '%physical-critical-chance':    'Physical Critical Chance',
+    '%special-critical-chance':     'Special Critical Chance',
+    '%critical-damage':             'Critical Damage',
+    '%potency':                     'Potency',
+    '%tenacity':                    'Tenacity',
+    '%health-steal':                'Health Steal',
+    '%protection':                  'Protection',
+    '%physical-accuracy':           'Physical Accuracy',
+    '%special-accuracy':            'Special Accuracy',
+    '%physical-critical-avoidance': 'Physical Critical Avoidance',
+    '%special-critical-avoidance':  'Special Critical Avoidance',
 }
 
 def local_time(date=None, timezone='Europe/Paris'):
-	if date is None:
-		date = datetime.now()
-	return pytz.timezone(timezone).localize(date)
+    if date is None:
+        date = datetime.now()
+    return pytz.timezone(timezone).localize(date)
 
 def http_get(url, headOnly=False):
 
-	try:
-		if headOnly is True:
-			response = requests.head(url)
-		else:
-			response = requests.get(url)
+    try:
+        if headOnly is True:
+            response = requests.head(url)
+        else:
+            response = requests.get(url)
 
-		response.raise_for_status()
+        response.raise_for_status()
 
-	except HTTPError as http_err:
-		return (None, 'HTTP error occured: %s' % http_err)
+    except HTTPError as http_err:
+        return (None, 'HTTP error occured: %s' % http_err)
 
-	except Exception as err:
-		return (None, 'Other error occured: %s' % err)
+    except Exception as err:
+        return (None, 'Other error occured: %s' % err)
 
-	else:
-		return response, False
+    else:
+        return response, False
 
 def http_post(url, *args, **kwargs):
 
-	try:
-		response = requests.post(url, *args, **kwargs)
-		if response.status_code not in [ 200, 404 ]:
-			response.raise_for_status()
+    try:
+        response = requests.post(url, *args, **kwargs)
+        if response.status_code not in [ 200, 404 ]:
+            response.raise_for_status()
 
-	except HTTPError as http_err:
-		return (None, 'HTTP error occured: %s' % http_err)
+    except HTTPError as http_err:
+        return (None, 'HTTP error occured: %s' % http_err)
 
-	except Exception as err:
-		return (None, 'Other error occured: %s' % err)
+    except Exception as err:
+        return (None, 'Other error occured: %s' % err)
 
-	else:
-		return response, False
+    else:
+        return response, False
 
 
 def get_units_dict(units, base_id_key):
@@ -108,7 +109,20 @@ def get_units_dict(units, base_id_key):
         for unit in units:
             base_id = str(unit[base_id_key])
             d[base_id] = unit
-    
+
         return d
-    
-   
+async def aiohttp_post(session,url, *args, **kwargs):
+    try:
+        async with session.post(url, *args, **kwargs) as response:
+                if response.status not in [200,404]:
+                    response.raise_for_status()
+                data = await response.json()
+
+    except HTTPError as http_err:
+        return (None, 'HTTP error occured: %s' % http_err)
+
+    except Exception as err:
+        return (None, 'Other error occured: %s' % err)
+
+    else:
+        return response, False
